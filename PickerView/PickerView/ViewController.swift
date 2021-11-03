ae6ff993055bb46a78495298ac0007302a35f8d7
wongil12 <judgevi52@gmail.com>
2021-11-03 23:24:34 +0900

    피커뷰에 이미지 띄우는 것 까지 완료(도전문제는 내일 완료하는걸로)

--- PickerView/PickerView/ViewController.swift
+++ PickerView/PickerView/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     let MAX_ARRAY_NUM = 10
     let PICKER_VIEW_COLUMN = 1
-    var imagefileName = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg"]
+    let PICKER_VIEW_HEIGHT:CGFloat = 80
+    var imageArray = [UIImage?]()
+    var imageFileName = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg"]
     
     @IBOutlet var pickerImage: UIPickerView!
     @IBOutlet var lblImageFileName: UILabel!
@@ -20,6 +22,16 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 0...9 하면 0부터 9까지의 Range를 설정한다.
+        // ..<를 하면 뒤에 숫자보다 작을 때 까지 실행한다.
+        for i in 0 ..< MAX_ARRAY_NUM {
+            let image = UIImage(named: imageFileName[i])
+            imageArray.append(image)
+        }
+        
+        lblImageFileName.text = imageFileName[0]
+        imageView.image = imageArray[0]
     }
     
     // 피커 뷰에게 컴포넌트의 수를 정수 값으로 넘겨주는 메서드
@@ -29,14 +41,32 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     // 피커 뷰에게 컴포넌트 열의 개수를 정수 값으로 넘겨주는 메서드
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return imagefileName.count
+        return imageFileName.count
     }
     
+    // picker View의 높이를 지정해주는 메서드
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return PICKER_VIEW_HEIGHT
+    }
+    
+    /*
     // 각 열의 타이틀을 문자열로 넘겨주는 메서드
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return imagefileName[row]
+        return imageFileName[row]
+    }
+     */
+    
+    // UIView를 return 하기 때문에 row에 이미지를 띄움
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let imageView = UIImageView(image:imageArray[row])
+        imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 150)
+        return imageView
     }
 
-
+    // 피커뷰의 룰렛을 돌려 열을 선택했을 때 실행되는 메서드
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        lblImageFileName.text = imageFileName[row]
+        imageView.image = imageArray[row]
+    }
 }
 
