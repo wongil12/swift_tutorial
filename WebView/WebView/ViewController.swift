18f4b214a69d8853a875b1743996c2bd3d32b58f
wongil12 <judgevi52@gmail.com>
2021-11-07 22:25:24 +0900

    웹뷰 로딩 추가 완료

--- WebView/WebView/ViewController.swift
+++ WebView/WebView/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
     @IBOutlet var txtUrl: UITextField!
     @IBOutlet var myWebView: WKWebView!
     @IBOutlet var myActivityIndicator: UIActivityIndicatorView!
@@ -24,8 +24,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        myWebView.navigationDelegate = self
         loadWebPage("http://naver.com")
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        myActivityIndicator.startAnimating()
+        myActivityIndicator.isHidden = false
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        myActivityIndicator.stopAnimating()
+        myActivityIndicator.isHidden = true
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        myActivityIndicator.stopAnimating()
+        myActivityIndicator.isHidden = true
     }
 
     @IBAction func btnGotoUrl(_ sender: UIButton) {
