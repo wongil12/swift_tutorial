a13fc89bfc168294e0058a0078d830ba41b05b91
wongil12 <judgevi52@gmail.com>
2022-01-31 17:21:51 +0900

    Map 완료

---
+++ Map/Map/ViewController.swift
@@ -0,0 +1,91 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by Wongil on 2022/01/31.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet var myMap: MKMapView!
+    @IBOutlet var lblLocationInfo1: UILabel!
+    @IBOutlet var lblLocationInfo2: UILabel!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        lblLocationInfo1.text = ""
+        lblLocationInfo2.text = ""
+        locationManager.delegate = self
+        // 정확도를 최고 상태로 설정
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        // 사용자에게 위치 정보 권한 승인 요청
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        myMap.showsUserLocation = true
+    }
+    
+    func goLocation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span :Double) -> CLLocationCoordinate2D {
+        let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
+        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+        let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
+        
+        myMap.setRegion(pRegion, animated: true)
+        return pLocation
+    }
+    
+    func setAnnotation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span :Double, title strTitle: String, subtitle strSubtitle: String) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = goLocation(latitudeValue: latitudeValue, longitudeValue: longitudeValue, delta: span)
+        annotation.title = strTitle
+        annotation.subtitle = strSubtitle
+        myMap.addAnnotation(annotation)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let pLocation = locations.last
+        _ = goLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
+        CLGeocoder().reverseGeocodeLocation(pLocation!, completionHandler: {
+            (placemarks, error) -> Void in
+            let pm = placemarks!.first
+            let country = pm!.country
+            var address:String = country!
+            if pm!.locality != nil {
+                address += " "
+                address += pm!.locality!
+            }
+            if pm!.thoroughfare != nil {
+                address += " "
+                address += pm!.thoroughfare!
+            }
+            
+            self.lblLocationInfo1.text = "현재 위치"
+            self.lblLocationInfo2.text = address
+            
+        })
+        
+        locationManager.stopUpdatingLocation()
+    }
+
+
+    @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            self.lblLocationInfo1.text = ""
+            self.lblLocationInfo2.text = ""
+            locationManager.startUpdatingLocation()
+        } else if sender.selectedSegmentIndex == 1 {
+            setAnnotation(latitudeValue: 37.579301, longitudeValue: 126.965477, delta: 0.01, title: "배화여자대학교", subtitle: "서울특별시 종로구 필운대로1길 34")
+            self.lblLocationInfo1.text = "보고 계신 위치"
+            self.lblLocationInfo2.text = "배화여자대학교"
+        } else if sender.selectedSegmentIndex == 2 {
+            setAnnotation(latitudeValue: 37.556876, longitudeValue: 126.914066, delta: 0.01, title: "이지스퍼블리싱", subtitle: "서울시 마포구 잔다리로 109 이지스 빌딩")
+            self.lblLocationInfo1.text = "보고 계신 위치"
+            self.lblLocationInfo2.text = "이지스퍼블리싱 출판사"
+        }
+    }
+}
+
