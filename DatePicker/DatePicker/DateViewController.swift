147a9a94de6e8807dbf83b993c2380b3ef8f5765
wongil12 <judgevi52@gmail.com>
2022-02-02 18:52:34 +0900

    Tab 연결 완료

---
+++ DatePicker/DatePicker/DateViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  DatePicker
+//
+//  Created by Wongil on 2021/11/02.
+//  Copyright © 2021 Won-Gil Kim. All rights reserved.
+//
+
+import UIKit
+
+class DateViewController: UIViewController {
+    let timeSelector: Selector = #selector(DateViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
+    @IBOutlet var lblCurrentTime: UILabel!
+    @IBOutlet var lblPickerTime: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // timeInterval: 타이머 간격
+        // target: 동작될 view
+        // selector: 타이머가 구동될 때 실행할 함수
+        // userInfo: 사용자 정보
+        // repeats: 반복 여부
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+    }
+
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        lblPickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+    }
+    @objc func updateTime() {
+//        lblCurrentTime.text = String(count)
+//        count += 1
+        // 현재 시간을 가져옴 JS에서 new Date() 같은거
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblCurrentTime.text = "현재시간: " + formatter.string(from: date as Date)
+    }
+}
+
---
+++ ImageView/ImageView/ImageViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  ImageView
+//
+//  Created by Wongil on 2021/11/01.
+//  Copyright © 2021 Won-Gil Kim. All rights reserved.
+//
+
+import UIKit
+
+// 변수 타입 뒤에 붙는 ?는 옵셔널 변수를 의미함
+// 옵셔널 변수는 변수의 값이 nil이거나 값의 존재 여부를 알 수 없다는 것을 의미한다.
+class ImageViewController: UIViewController {
+    // 확대 여부 변수
+    var isZoom = false
+    // 켜진 전구 이미지가 저장되는 변수
+    var imgOn: UIImage?
+    // 꺼진 전구 이미지가 저장되는 변수
+    var imgOff: UIImage?
+    
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var btnResize: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        imgOn = UIImage(named: "lamp_on.png")
+        imgOff = UIImage(named: "lamp_off.png")
+        
+        imgView.image = imgOn
+    }
+    // 확대 버튼 클릭 시 실행되는 함수
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+        let scale:CGFloat = 2.0
+        var newWidth:CGFloat, newHeight: CGFloat
+        
+        if(isZoom) {
+            newWidth = imgView.frame.width/scale
+            newHeight = imgView.frame.height/scale
+            btnResize.setTitle("확대", for: .normal)
+        } else {
+            newWidth = imgView.frame.width*scale
+            newHeight = imgView.frame.height*scale
+            btnResize.setTitle("축소", for: .normal)
+        }
+        imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+        
+        isZoom = !isZoom
+    }
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            imgView.image = imgOn
+        } else {
+            imgView.image = imgOff
+        }
+    }
+    
+}
+
