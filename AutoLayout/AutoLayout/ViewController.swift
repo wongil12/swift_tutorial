19ce932a420bb881727034f2e8856e24dbca25a2
wongil12 <judgevi52@gmail.com>
2022-02-09 22:47:32 +0900

    autolayout & SafeArea 개념 익히기

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Wongil on 2022/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
