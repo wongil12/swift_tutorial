51fe538c31bffb67a83a93104b8c01e92dd5ad4d
wongil12 <judgevi52@gmail.com>
2022-02-21 22:41:38 +0900

    Outlet 변수 action 함수 추가 완료

--- CameraPhotoLibrary/CameraPhotoLibrary/ViewController.swift
+++ CameraPhotoLibrary/CameraPhotoLibrary/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func btnCaptureImageFromCamera(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnLoadImageFromLibrary(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnRecordVideoFromCamera(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnLoadVideoFromLibrary(_ sender: UIButton) {
+    }
+    
 }
 
