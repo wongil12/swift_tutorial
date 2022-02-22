a87375eb3067e38de9dff1322a8a5ab442431cef
wongil12 <judgevi52@gmail.com>
2022-02-22 22:19:39 +0900

    outlet 변수, action 함수 연결

--- DrawGraphics/DrawGraphics/ViewController.swift
+++ DrawGraphics/DrawGraphics/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnDrawLine(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnDrawRectangle(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnDrawCircle(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnDrawArc(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnDrawFill(_ sender: UIButton) {
+    }
 }
 
