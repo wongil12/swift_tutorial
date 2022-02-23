0f26e264b804cb706aa4a2395dd33104ef2b7ebc
wongil12 <judgevi52@gmail.com>
2022-02-23 22:40:04 +0900

    sketch 앱 레이아웃 구성 및 outlet 변수, action 함수 연결 완료

---
+++ Sketch/Sketch/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Sketch
+//
+//  Created by Wongil on 2022/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var imgView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func clearImageView(_ sender: UIButton) {
+    }
+    
+}
+
