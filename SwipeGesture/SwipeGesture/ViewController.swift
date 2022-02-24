ab8477d4e48de4d404e2d9776b067a8222564320
wongil12 <judgevi52@gmail.com>
2022-02-24 22:44:35 +0900

    스와이프 제스처 스토리보드 구성 및 outlet 변수 연결

---
+++ SwipeGesture/SwipeGesture/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SwipeGesture
+//
+//  Created by Wongil on 2022/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var imgViewUp: UIImageView!
+    @IBOutlet var imgViewDown: UIImageView!
+    @IBOutlet var imgViewLeft: UIImageView!
+    @IBOutlet var imgViewRight: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
